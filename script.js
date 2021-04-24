337b376207c862ab4d984181bf349269151af5b1
budds300 <budds300@gmail.com>
2021-04-24 19:36:37 +0300

    js Bado

--- script.js
+++ script.js
@@ -6,30 +6,65 @@ function Pizza(size, crust, toppings){
 function Price(pizzaSizePrice,pizzaCrustPrice,pizzaToppingsPrice){
     this.pizzaSizePrice = pizzaSizePrice;
     this.pizzaCrustPrice = pizzaCrustPrice;
-    this.toppings = pizzaToppingsPrice;
+    this.pizzaToppingsPrice = pizzaToppingsPrice;
 }
 Price.prototype.getPrice = function(){
     return this.pizzaSizePrice + this.pizzaCrustPrice + this.pizzaToppingsPrice;
 }
+const flavor = ["Peri Peri Chicken","Hawaiian","Boerewors Pizza","BBQ Steak Pizza","Chicken Tikka Pizza","Chicken and Mushroom","Margherita Pizza","Regina Pizza"];
 const pizzaSize = ["small","medium","Large"];
 const pizzaCrust = ["crispy","stuffed","glutten free"]
 const pizzaToppings = ["peperroni","mushrooms","onions","sausage","bacon","extra-cheese","green-pepper"];
 const sizePrice = [600,800,1000];
 const crustPrice = [250,300,200];
 const toppingsPrice = [50,50,50,100,150,100,50];
+$("document").ready(function(){
+
 let form = document.getElementById("pizza");
 form.addEventListener("submit", (e) =>{
-    const sizePizza = document.getElementById("pizza-size").value;
-    const curstPizza = document.getElementById("crust").value;
-    const toppingsPizza = document.getElementById("toppings").value;
+    e.preventDefault();
+    const flavorPizza = parseInt(document.getElementById("flavor"));
+    const sizePizza = parseInt(document.getElementById("pizza-size").value);
+    const curstPizza = parseInt(document.getElementById("crust").value);
+    const toppingsPizza =parseInt(document.getElementById("topping").value);
+    let flavorOrder = flavor[flavorPizza-1]
     let sizePizzaOrder= pizzaSize[sizePizza-1];
     let crustPizzaOrder= pizzaCrust[curstPizza-1];
     let toppingsPizzaOrder= pizzaToppings[toppingsPizza-1];
-    let customerOrder= new Pizza(sizePizzaOrdery,toppingsPizzaOrder,crustPizzaOrder);
-    const sizePriceOrder = sizePrice[sizePrice-1];
+    let customerOrder= new Pizza(flavorOrder,sizePizzaOrder,toppingsPizzaOrder,crustPizzaOrder);
+    console.log(customerOrder);
+    const sizePriceOrder = sizePrice[sizePizza-1];
     const crustPriceOrder = crustPrice[curstPizza-1];
-    const toppingsPriceOrder = toppingsPizza[toppingsPrice-1];
+    const toppingsPriceOrder = toppingsPrice[toppingsPizza-1];
+    let deliveryCost= document.getElementById("delivery").value;
     
-    let customerPrice = new Price(sizePizzaOrder,crustPizzaOrder,toppingsPizzaOrder);
     
+    let customerCostPrice = new Price(sizePriceOrder,crustPriceOrder,toppingsPriceOrder);
+    console.log(customerCostPrice, customerCostPrice.getPrice());
+    const quantity = parseInt(document.getElementById("quantity").value);
+
+    if (quantity >= 1 && deliveryCost){
+        if (deliveryCost=="delivery") {
+            let deliveryLocation= prompt("Enter Your Location");
+            alert("Hello your order is"+ customerOrder+". It will be delivered to you shortly at"+ deliveryLocation+ "Thank-you")
+             console.log(customerCostPrice.getPrice()*quantity + 300);
+            
+        }
+        else if (deliveryCost=="collection") {
+            console.log(customerCostPrice.getPrice()*quantity);
+            
+        }
+        else {
+            alert("Wacha Jokes Msee")
+        }
+    }else{
+        console.log("Kwani iko NINI!?")
+    }
+        
+    })
+
 } )
+
+    
+
+

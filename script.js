501cdfc2e53ab5e02fd76b08710092b928cec478
budds300 <budds300@gmail.com>
2021-04-24 12:47:48 +0300

    constant introduction

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ function Price(pizzaSizePrice,pizzaCrustPrice,pizzaToppingsPrice){
     this.pizzaCrustPrice = pizzaCrustPrice;
     this.toppings = pizzaToppingsPrice;
 }
+Price.prototype.getPrice = function(){
+    return this.pizzaSizePrice + this.pizzaCrustPrice + this.pizzaToppingsPrice;
+}
+
+form.addEventListener("submit", (e) =>{
+    const sizePizza = document.getElementById("pizza-size").value;
+    const curstPizza = document.getElementById("crust").value;
+    const toppingsPizza = document.getElementById("toppings").value;
+    
+} )

081cb2159300c97bc4b712f1fb10b3b3b33e8d58
budds300 <budds300@gmail.com>
2021-04-24 12:47:07 +0300

    initial js

---
+++ script.js
@@ -0,0 +1,10 @@
+function Pizza(size, crust, toppings){
+    this.size = size;
+    this.crust = crust;
+    this.toppings = toppings;
+}
+function Price(pizzaSizePrice,pizzaCrustPrice,pizzaToppingsPrice){
+    this.pizzaSizePrice = pizzaSizePrice;
+    this.pizzaCrustPrice = pizzaCrustPrice;
+    this.toppings = pizzaToppingsPrice;
+}

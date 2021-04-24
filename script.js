a328d83fa72d6fc100a91be23f7985f81a02f935
budds300 <budds300@gmail.com>
2021-04-24 13:51:12 +0300

    eventListener

--- script.js
+++ script.js
@@ -11,10 +11,12 @@ function Price(pizzaSizePrice,pizzaCrustPrice,pizzaToppingsPrice){
 Price.prototype.getPrice = function(){
     return this.pizzaSizePrice + this.pizzaCrustPrice + this.pizzaToppingsPrice;
 }
+const pizzaSize = ["small","medium","Large"];
+const pizzaCrust = ["crispy","stuffed","glutten free"]
+const pizzaToppings = ["peperroni","mushrooms","onions","sausage","bacon","extra-cheese","green-pepper"];
+const sizePrice = [600,800,1000];
+const crustPrice = [250,300,200];
+const toppingsPrice = [50,50,50,100,150,100,50];
+let form = document.getElementById("pizza");
 
-form.addEventListener("submit", (e) =>{
-    const sizePizza = document.getElementById("pizza-size").value;
-    const curstPizza = document.getElementById("crust").value;
-    const toppingsPizza = document.getElementById("toppings").value;
-    
 } )
